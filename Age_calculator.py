1c7f38586d1a4105c96500b13130c03f0c25d37c
alsaiyh-ai <okshialsaiyh@gmail.com>
2026-07-28 08:27:14 +0200

    Add files via upload

---
+++ Age_calculator.py
@@ -0,0 +1,25 @@
+age=int(input("What’s Your Age ?"))
+months=age*12
+days=age*365
+weeks=days//7
+hours=days*24
+minutes=hours*602
+seconds=minutes*60
+
+print("You lived for:- ")
+print(f"{months} months")
+print(f"{weeks} weeks")
+print(f"{days} days")
+print(f"{hours:,} hours")   
+print(f"{minutes:,} minutes")
+print(f"{seconds:,} seconds")
+
+
+
+
+
+
+
+
+
+
